f4b8c28c98a70e300225f2d2673b0d11b3e01888
Justin Veach <justinwveach@gmail.com>
2018-03-13 10:57:55 -0500

    Added api to add/remove emails from newsletter table

---
+++ cancel-newsletter.js
@@ -0,0 +1,19 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: `${process.env.TABLE_PREFIX}newsletter`,
+    Key: {
+      email: event.pathParameters.email
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("delete", params);
+    callback(null, success({ status: true }));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
---
+++ signup-newsletter.js
@@ -0,0 +1,21 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: `${process.env.TABLE_PREFIX}newsletter`,
+    Item: {
+      email: data.email,
+      createdAt: new Date().getTime()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
